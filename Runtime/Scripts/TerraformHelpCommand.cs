fbbe26d131568e68e8a4af19ff67a61b8ca7abac
Judsin1349 <judsin1349@gmail.com>
2025-11-16 23:08:40 -0600

    added some terraforming commands

---
+++ Runtime/Scripts/TerraformHelpCommand.cs
@@ -0,0 +1,32 @@
+﻿// Copyright 2025 Spellbound Studio Inc.
+
+using System.Text;
+using Spellbound.Core.Console;
+
+namespace Spellbound.MarchingCubes {
+    /// <summary>
+    /// Help command that lists all terraform-related utility commands.
+    /// </summary>
+    [ConsoleCommandClass("terraform", "tf")]
+    public class TerraformHelpCommand : ICommand {
+        public string Name => "terraform";
+        public string Description => "List all terraform commands";
+        public string Usage => "terraform";
+
+        public CommandResult Execute(string[] args) {
+            // Get all commands from SbTerrain class
+            var commands = 
+                    PresetCommandRegistry.GetUtilityCommandsByClass(typeof(SbTerrain));
+
+            var sb = new StringBuilder();
+            sb.AppendLine("=== Terraform Commands ===");
+            sb.AppendLine();
+
+            foreach (var (commandName, description) in commands) {
+                sb.AppendLine($"{commandName,-25} {description}");
+            }
+
+            return CommandResult.Ok(sb.ToString());
+        }
+    }
+}
