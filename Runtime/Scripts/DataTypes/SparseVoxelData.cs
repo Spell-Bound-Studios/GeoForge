98abc45ffb1a77dc482f86eec72d9d6e8a4535a5
messdp <dpmessenger15@gmail.com>
2026-09-03 16:08:15 -0700

    this time for sure

--- Runtime/Scripts/DataTypes/SparseVoxelData.cs
+++ Runtime/Scripts/DataTypes/SparseVoxelData.cs
@@ -1,19 +1,25 @@
 // Copyright 2026 Spellbound Studio Inc.
 
+using System;
+using UnityEngine;
+
 namespace Spellbound.GeoForge {
     /// <summary>
     /// Represents a run of the same voxels.
     /// A NativeList of these structs can represent the full voxel data of a geoChunk within less memory.
     /// The Marching Cubes Algorithm CANNOT operate on this representation of voxel data.
     /// It must be decompressed for marching.
-    /// </summary>
+    ///
+    [Serializable]
     public struct SparseVoxelData {
         public VoxelData Voxel;
-        public readonly int StartIndex;
+
+        [SerializeField] private int startIndex;
+        public int StartIndex => startIndex;
 
         public SparseVoxelData(VoxelData voxel, int startIndex) {
             Voxel = voxel;
-            StartIndex = startIndex;
+            this.startIndex = startIndex;
         }
     }
 }
